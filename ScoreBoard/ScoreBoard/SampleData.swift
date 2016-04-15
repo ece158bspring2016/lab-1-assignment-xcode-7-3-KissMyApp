d0f0b6c29de8092c38f1a554aefca6d7b414590f
William Quan <wiquan@ucsd.edu>
2016-04-14 22:10:48 -0700

    Finished tutorial adding more of what we want on our own study buddy app names, rankings, titles

---
+++ ScoreBoard/ScoreBoard/SampleData.swift
@@ -0,0 +1,15 @@
+//
+//  SampleData.swift
+//  ScoreBoard
+//
+//  Created by William Quan on 4/14/16.
+//  Copyright © 2016 William Quan. All rights reserved.
+//
+
+import Foundation
+
+let playersData = [
+    Player(name: "Will Quanster", game: "Study God", rating: 5),
+    Player(name: "Lilli Swagstaff", game: "Please Uninstall", rating: 1),
+    Player(name: "Katie DTR", game: "Study Scrub", rating: 2)
+]
